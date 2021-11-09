5e3cceaee4a76305b82b485510648d00abf9fa34
rajat566 <87006997+rajat566@users.noreply.github.com>
2021-11-09 23:21:43 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="scr.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+</head>
+
+    <body>
+        
+        </script>
+        <div style="font-size: 200%;">Registration closes in <span id="time">3:00</span> minutes!</div>
+
+        <form align="center">
+
+<h2 style="font-size: 200%;">Registration Form</h2><br>
+
+Name: <input type="text" name="name"><br><br>
+E-mail: <input type="text" name="email"><br>
+
+<label for="start"><br>
+        Date of Birth:
+    </label>
+  
+    <input type="date" name="begin" 
+        placeholder="dd-mm-yyyy" value=""
+        min="1997-01-01" max="2030-12-31"><br><br>
+        About yourself: <textarea name="message" rows="5" cols="30">
+</textarea> <br><br>
+<div align="center" class="g-recaptcha" data-sitekey="6LeftiIdAAAAAMfVsRyJ0jkY9O59L-z5VRscm7av"></div>
+<br><br>        
+<input type="submit">
+
+</form>
+
+
+    </body>
+    
+
+</html>
